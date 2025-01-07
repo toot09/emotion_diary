b48b10683d1c9c861ce9214f58cea5da258bc691
i202202035 <i202202035@hanwha.com>
2025-01-07 17:07:34 +0900

    New 페이지 구현하기 (기능) - !!저장후 신규 일기 조회되지 않는 이슈 해결필요

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,6 @@
 import "./Editor.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import EmotionItem from "./EmotionItem";
 import Button from "./Button";
 
@@ -25,35 +27,92 @@ const emotionList = [
   },
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+const getStringedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ onSubmit }) => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const nav = useNavigate();
+
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+    setInput({ ...input, [name]: value });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  };
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
+
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
         <div className="emotion_list_wrapper">
           {emotionList.map((emotion) => (
             <EmotionItem
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: emotion.emotionId,
+                  },
+                })
+              }
               key={emotion.emotionId}
               {...emotion}
-              isSelected={emotion.emotionId === emotionId}
+              isSelected={emotion.emotionId === input.emotionId}
             />
           ))}
         </div>
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+          placeholder="오늘은 어땠나요?"
+        />
       </section>
 
       <section className="button_section">
-        <Button text={"취소"} />
-        <Button text={"저장"} type={"POSITIVE"} />
+        <Button
+          onClick={() => {
+            nav(-1);
+          }}
+          text={"취소"}
+        />
+        <Button onClick={onClickSubmitButton} text={"저장"} type={"POSITIVE"} />
       </section>
     </div>
   );
